b7efd9e9a3cfeb46ba0c5e3b293708216d441ad8
janboode <janboode@gmail.com>
2019-08-04 16:20:01 +0200

    Added better styles

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './App.scss'
 
 const App = () => {
     return(
-        <div>
+        <div className="App">
             <h1>2019 basic react boilerplate.</h1>
-            <img src="../assets/images/me_01.png" alt="bla" />
+            <img src="../assets/images/me_01.png" alt="bla" style={{width: "300px"}} />
         </div>
     )
 }
