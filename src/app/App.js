1d5e5432d9fa394b6804d3174f122096e32116a0
janboode <janboode@gmail.com>
2019-08-04 14:09:13 +0200

    added new files

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// const App = () => {
+export default App = () => {
+    return(
+        <div>
+            <h1>2019 basic react boilerplate.</h1>
+            <img src="../assets/images/me_01.png" alt="bla" />
+        </div>
+    )
+}
+
+// export default App
