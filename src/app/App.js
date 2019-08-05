1fb0f142acc008559b999462cab75ee4fe428ae2
janboode <janboode@gmail.com>
2019-08-05 12:36:50 +0200

    removed lorem

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,7 @@ const App = () => {
         <div className="App">
             <img src="../assets/images/me_01.png" alt="bla" style={{ width: "250px" }} />
             <h1>2019 basic react boilerplate.</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur nobis voluptate reprehenderit? Officiis odit sunt sequi distinctio laboriosam asperiores voluptatibus.</p>
+            <p>Personal Boilerplate for mid 2019. It contains Webpack, Babel, Sass, React and some goodies like post-css (autoprefixer), uglify-js and optimize-css! Ready when you are!</p>
         </div>
     )
 }
