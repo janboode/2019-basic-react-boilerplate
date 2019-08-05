2bfd35a516e1640bb20ef5653d5da1a3567c56ae
janboode <janboode@gmail.com>
2019-08-05 12:28:55 +0200

    style edits

--- src/app/App.js
+++ src/app/App.js
@@ -6,6 +6,7 @@ const App = () => {
         <div className="App">
             <img src="../assets/images/me_01.png" alt="bla" style={{ width: "250px" }} />
             <h1>2019 basic react boilerplate.</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur nobis voluptate reprehenderit? Officiis odit sunt sequi distinctio laboriosam asperiores voluptatibus.</p>
         </div>
     )
 }
