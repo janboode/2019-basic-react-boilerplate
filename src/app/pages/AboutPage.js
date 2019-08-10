4703db68ef7ce948b9c67aac2d412c204c686d7c
janboode <janboode@gmail.com>
2019-08-10 16:56:14 +0200

    added svg file

--- src/app/pages/AboutPage.js
+++ src/app/pages/AboutPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Logo from '../../assets/svg/crown.svg'
+
 class AboutPage extends React.Component {
     constructor(props) {
         super(props)
@@ -13,6 +15,7 @@ class AboutPage extends React.Component {
 
         return (
             <div className='page about'>
+                <Logo style={{marginBottom: '20px'}} />
                 <img src="../assets/images/me_01.png" alt="bla" style={{ width: "250px", transform: 'rotate(180deg)' }} />
                 <h1>This is the about page</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam repudiandae voluptate, iste tempore ipsum fuga? Cum delectus voluptatibus adipisci hic.</p>
