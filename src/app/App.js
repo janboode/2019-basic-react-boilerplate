abca4f740e117ff598bb09b3587b172302c15f2b
janboode <janboode@gmail.com>
2019-08-11 16:04:55 +0200

    added custom console message

--- src/app/App.js
+++ src/app/App.js
@@ -5,6 +5,21 @@ import AboutPage from './pages/AboutPage'
 import './App.scss'
 
 const App = () => {
+    const say = 'B R Y T E'
+    const spacing = '7px';
+    const borderColor = '#eae7ff'
+    const styles =
+        `
+                padding: ${spacing};
+                background-color: darkslateblue;
+                color: white;
+                font-style: normal;
+                font-weight: bold;
+                border: 2px solid ${borderColor};
+                font-size: 1em;
+                border-radius: 3px
+                `
+    console.log(`%c${say}`, styles)
     return (
         <div className="App">
             <Switch>
