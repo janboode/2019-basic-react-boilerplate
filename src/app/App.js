7bde5a3d2b86e643ff5889d0ab511b77c0befab6
janboode <janboode@gmail.com>
2019-10-21 16:13:49 +0200

    console msg

--- src/app/App.js
+++ src/app/App.js
@@ -6,21 +6,6 @@ import AboutPage from './pages/about-page/about-page.comp'
 import style from './App.scss'
 
 const App = () => {
-    const say = 'B R Y T E'
-    const spacing = '7px';
-    const borderColor = '#eae7ff'
-    const styles =
-        `
-            padding: ${spacing};
-            background-color: darkslateblue;
-            color: white;
-            font-style: normal;
-            font-weight: bold;
-            border: 2px solid ${borderColor};
-            font-size: 1em;
-            border-radius: 3px
-        `
-    console.log(`%c${say}`, styles)
     return (
         <div className={style.App}>
             {/* <Switch> */}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+const say = 'B R Y T E'
+const spacing = '7px';
+const borderColor = '#eae7ff'
+const styles =
+    `
+    padding: ${spacing};
+    background-color: darkslateblue;
+    color: white;
+    font-style: normal;
+    font-weight: bold;
+    border: 2px solid ${borderColor};
+    font-size: 1em;
+    border-radius: 3px;
+`
+console.log(`%c${say}`, styles)
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app/App'
